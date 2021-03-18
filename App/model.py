62d6a5ce1694f4606336b21c7697b77919d319da
HelenaVegalara <h.vegalara@uniandes.edu.co>
2021-03-17 21:22:44 -0500

    Guardado 2

--- App/model.py
+++ App/model.py
@@ -30,6 +30,11 @@ from DISClib.ADT import list as lt
 from DISClib.ADT import map as mp
 from DISClib.DataStructures import mapentry as me
 from DISClib.Algorithms.Sorting import shellsort as sa
+from DISClib.Algorithms.Sorting import insertionsort as ins
+from DISClib.Algorithms.Sorting import selectionsort as sele
+from DISClib.Algorithms.Sorting import mergesort as merge
+from DISClib.Algorithms.Sorting import quicksort as quick
+import time
 assert cf
 
 """
@@ -38,13 +43,86 @@ los mismos.
 """
 
 # Construccion de modelos
+def newCatalog(estructuraDeDatos):
+    catalog = {'videos': None,
+               'categories': None,
+               'country':None,
+               'likes':None}
+    
+    catalog['videos'] = lt.newList(estructuraDeDatos,
+                                    cmpfunction= cmpVideosByViews)
+    catalog['categories'] = mp.newMap(10000,
+                                      maptype= 'CHAINING',
+                                      loadfactor= 4.0,
+                                      comparefunction=compareMapCategories)
+    catalog['country'] = lt.newList(estructuraDeDatos)
+    catalog['likes'] = lt.newList(estructuraDeDatos)
+    return catalog
+
+
 
 # Funciones para agregar informacion al catalogo
+def addCategory(catalog, elem):
+    lt.addLast(catalog['categories'], elem)
+
+def addVideo(catalog, elem):
+    lt.addLast(catalog['videos'],elem)
 
 # Funciones para creacion de datos
+def newCategory(id):
+    catalog = {'id':''}
+    catalog['id'] = id
+    return catalog
+
+def newVideo(title, id):
+    video = {'title':'','video_id':''}
+    video['title'] = title
+    video['video_id'] = id
+    return video
+
 
 # Funciones de consulta
 
+
 # Funciones utilizadas para comparar elementos dentro de una lista
+def cmpVideosByViews(video1, video2):
+    #Retorna True si los 'views' del video1 son menores que los del video2
+    if float(video1['views']) > float(video2['views']):
+        return True
+    else:
+        return False
+
+def compareMapCategories(category1, category2):
+    pass
+
+
 
 # Funciones de ordenamiento
+def mejoresVideosPorViews(catalog, numeroDeElementos, algoritmo):
+    sub_list = lt.subList(catalog['videos'], 1, numeroDeElementos)
+    sub_list = sub_list.copy()
+    start_time = time.process_time()
+    if algoritmo == "shellsort":
+        sorted_list = sa.sort(sub_list, cmpVideosByViews)
+    elif algoritmo == "insertionsort":
+        sorted_list = ins.sort(sub_list, cmpVideosByViews)
+    elif algoritmo == "selectionsort":
+        sorted_list = sele.sort(sub_list, cmpVideosByViews)
+    elif algoritmo == "mergesort":
+        sorted_list = merge.sort(sub_list, cmpVideosByViews)
+    elif algoritmo == "quicksort":
+        sorted_list = quick.sort(sub_list, cmpVideosByViews)
+    stop_time = time.process_time()
+    elapsed_time_mseg = (stop_time -start_time)*1000
+    return elapsed_time_mseg, sorted_list
+
+
+def videostendenciaporpais(catalog, categorias, pais,numero):
+
+    sub_list = lt.subList(catalog['videos'], 1, (numero+1))
+    sub_list = sub_list.copy()
+    sorted_list = sa.sort(sub_list,cmpVideosByViews)
+    for i in sorted_list['country']:
+        paises = lt.getElement(sorted_list['country'], i)
+        lt.addFirst(sorted_list,paises)
+    return sorted_list 
