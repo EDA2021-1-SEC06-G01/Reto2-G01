5f44947c3fdef48bccef8354a481959eecaec2a1
j.rodriguez26 <j.rodriguez26@uniandes.edu.co>
2021-04-14 23:10:19 -0500

    guardado 8

--- App/controller.py
+++ App/controller.py
@@ -23,7 +23,8 @@
 import config as cf
 import model
 import csv
-"www"
+import time
+import tracemalloc
 
 """
 El controlador se encarga de mediar entre la vista y el modelo.
@@ -35,11 +36,26 @@ def initCatalog(estructuraDeDatos):
 
 # Funciones para la carga de datos
 def loadData(catalog):
+    delta_time = -1.0
+    delta_memory = -1.0
+
+    tracemalloc.start()
+    start_time = getTime()
+    start_memory = getMemory()
+
     loadCategory(catalog)
     loadVideos(catalog)
+    stop_memory = getMemory()
+    stop_time = getTime()
+    tracemalloc.stop()
+
+    delta_time = stop_time - start_time
+    delta_memory = deltaMemory(start_memory, stop_memory)
+
+    return delta_time, delta_memory
 
 def loadCategory(catalog):
-    category = cf.data_dir + '/category-id.csv'
+    category = cf.data_dir + 'category-id.csv'
     input_file = csv.DictReader(open(category, encoding='utf-8'), delimiter = "\t")
     for c in input_file:
         model.addCategory(catalog, c)
@@ -50,6 +66,37 @@ def loadVideos(catalog):
     for video in input_file:
         model.addVideo(catalog, video)
 
+
+
+def getTime():
+    """
+    devuelve el instante tiempo de procesamiento en milisegundos
+    """
+    return float(time.perf_counter()*1000)
+
+
+def getMemory():
+    """
+    toma una muestra de la memoria alocada en instante de tiempo
+    """
+    return tracemalloc.take_snapshot()
+
+
+def deltaMemory(start_memory, stop_memory):
+    """
+    calcula la diferencia en memoria alocada del programa entre dos
+    instantes de tiempo y devuelve el resultado en bytes (ej.: 2100.0 B)
+    """
+    memory_diff = stop_memory.compare_to(start_memory, "filename")
+    delta_memory = 0.0
+
+    # suma de las diferencias en uso de memoria
+    for stat in memory_diff:
+        delta_memory = delta_memory + stat.size_diff
+    # de Byte -> kByte
+    delta_memory = delta_memory/1024.0
+    return delta_memory
+
 # Funciones de ordenamiento
 
 # Funciones de consulta sobre el catálogo
@@ -57,6 +104,12 @@ def mejoresVideosPorViews(catalog, numeroDeElementos, algoritmo):
     resultado = model.mejoresVideosPorViews(catalog, numeroDeElementos, algoritmo)
     return resultado
 
-def videos_tendencia_por_pais(catalog,categoria,ciudad,numero):
-    resultado = model.videostendenciaporpais(catalog,categoria,ciudad,numero)
+def videosTendenciaPorpais(catalog,pais):
+    
+    resultado = model.videostendenciaporpais(catalog,pais)
+    return resultado
+
+def videosTendenciaPorcategoria(catalog,pais):
+    
+    resultado = model.videosporcategoria(catalog,pais)
     return resultado
